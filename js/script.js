c2f54cb918c72ca1e9c82bf9cb2bb6527215f551
vladislavkoronchik <81505769+vladislavkoronchik@users.noreply.github.com>
2021-05-28 14:00:32 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var instance = new vidbg('.video', {
+	mp4: 'video/world.mp4',
+	webm: 'video/world.webm',
+	poster: 'video/poster.jpg'
+});
+var rellax = new Rellax('.rellax');
+var aos = AOS.init();
